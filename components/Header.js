7febc30be55b14afacba197f1c635c0bd663fe87
Bryan Fullam <bryanfullam@gmail.com>
2024-07-14 00:27:40 +0200

    Done

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,13 @@ export default function Header() {
     <div className="fixed top-0 w-full z-30 clearNav md:bg-opacity-90 transition duration-300 ease-in-out">
       <div className="flex flex-col max-w-6xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between p-4">
+          <img src="/images/logo.jpg" alt="logo" className="w-12 h-12" />
           <a
             href="/"
             className="text-lg font-semibold rounded-lg tracking-widest focus:outline-none focus:shadow-outline"
           >
             <h1 className="text-4xl Avenir tracking-tighter text-gray-900 md:text-4x1 lg:text-3xl">
-              OnlyFarts
+              {" "}OnlyFarts
             </h1>
           </a>
           <button
@@ -45,44 +46,6 @@ export default function Header() {
             (navbarOpen ? " flex" : " hidden")
           }
         >
-          <nav className="flex-col flex-grow ">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li>
-                <a
-                  href="/"
-                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Careers
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
-                  href="/"
-                >
-                  <span className="justify-center">Download</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
     </div>
--- components/Main.js
+++ components/Main.js
@@ -12,105 +12,20 @@ export default function Main() {
           <div className="flex justify-center">
             <a
               className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-              href="https://github.com/r1/nine4-2/"
             >
-              <span className="justify-center">Download</span>
+              <span className="justify-center">Join the waitlist</span>
             </a>
           </div>
         </div>
         <div className="xl:mr-44 sm:mr-0 sm:mb-28 mb-0 lg:mb-0 mr-48 md:pl-10">
           <img
             className="md:ml-1 ml-24"
-            style={{ width: "38rem" }}
+            style={{ width: "34rem" }}
             alt="iPhone-12"
-            src="/images/iPhone-12-Mockup.png"
-          ></img>
-          <div className="carousel">
-            <img src="/images/carousel-image1.png" alt="Carousel Image 1" />
-            <img src="/images/carousel-image2.png" alt="Carousel Image 2" />
-            <img src="/images/carousel-image3.png" alt="Carousel Image 3" />
-          </div>
-        </div>
-      </div>
-      <section className="mx-auto">
-        <div className="container px-5 mx-auto lg:px-24 ">
-          <div className="flex flex-col w-full mb-4 text-left lg:text-center">
-            <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
-              Trusted by top-tier product companies
-            </h1>
-          </div>
-          <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Google-Logo.webp"
-                alt="Google Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Shopify-Logo.svg"
-                alt="Shopify Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/Cloudflare-Logo.svg"
-                alt="Cloudflare Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-            <div className="flex items-center justify-center">
-              <img
-                src="/images/PayPal-Logo.png"
-                alt="Paypal Logo"
-                className="block object-contain h-16 greyC"
-              ></img>
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="grr max-w-7xl pt-20 mx-auto text-center">
-        <h1 className="mb-8 text-6xl Avenir font-semibold text-gray-900">
-          Less code, less effort.
-        </h1>
-        <h1 className="mb-8 text-2xl Avenir font-semibold text-gray-600 text-center">
-          Minify your CSS with Tailwind's built in PostCSS support.
-        </h1>
-        <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
-          <img
-            className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
-            alt="Placeholder Image"
-            src="./images/placeholder.png"
+            src="/images/mockup.jpg"
           ></img>
         </div>
       </div>
-      <section className="relative">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <div className="py-24 md:py-36">
-            <h1 className="mb-5 text-6xl Avenir font-semibold text-gray-900">
-              Subscribe to our newsletter
-            </h1>
-            <h1 className="mb-9 text-2xl font-semibold text-gray-600">
-              Enter your email address and get our newsletters straight away.
-            </h1>
-            <input
-              placeholder="jack@example.com"
-              name="email"
-              type="email"
-              autoComplete="email"
-              className="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-900"
-            ></input>{" "}
-            <a
-              className="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
-              href="/"
-            >
-              <span className="justify-center">Subscribe</span>
-            </a>
-          </div>
-        </div>
-      </section>
     </section>
   );
 }
